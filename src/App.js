a9ffe2157e310fd0ed83268a1c45913ce5586d62
Gazpacho <Gazpachoblaze@gmail.com>
2023-09-28 14:48:45 -0500

    changing states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Item(props) {
+  return <div className="item">
+  <b>Name: </b> {props.name }
+  <b> Price: </b> ${props.price } <br />
+  </div>;
 }
+
+function App() {
+  return <div>
+    <Item name="Cheese" price="4" />
+    <Item name="Eggs" price="3" />
+    <Item name="Rice" price="18" />
+  </div>;
+}
+
+
 
 export default App;
